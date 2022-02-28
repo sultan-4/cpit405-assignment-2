f0c5539299382f0e1ee31072e35df79e0a6b1db8
sultan-4 <sultan.alhazmi11@gmail.com>
2022-02-28 14:55:07 +0300

    Cookies

--- script.js
+++ script.js
@@ -1,24 +1,31 @@
-const liItem = document.querySelectorAll('ul li');
-const imgItem = document.querySelectorAll('.image img');
+var liItem = document.querySelectorAll('ul li');
+var imgItem = document.querySelectorAll('.image img');
 
 liItem.forEach(li => {
-   li.onclick = function() {
-   
+  li.onclick = function () {
     liItem.forEach(li => {
-        li.className = "";
+      li.className = "";
     })
     li.className = "all";
-
-   
-    const value = li.textContent;
-    console.log(value)
-    imgItem.forEach(img => {
-        img.style.display = 'none';
-        if (img.getAttribute('data-filter') == value || value == "All") {
-            img.style.display = '';
-
-        }
-    })
-   }
+    var value = li.textContent;
+    document.cookie = 'liselect=' + value 
+    
+    showImage(imgItem,value)
+  }
 });
 
+window.onload = function () {
+  if (document.cookie != undefined) {
+      showImage(imgItem,document.cookie.split("=")[1])
+      console.log(document.cookie)
+  }
+}
+function showImage (imgItem ,value){
+  imgItem.forEach(img => {
+    img.style.display = 'none';
+    if (img.getAttribute('data-filter') == value || value == "All") {
+      img.style.display = '';
+    }
+    
+  })
+}
