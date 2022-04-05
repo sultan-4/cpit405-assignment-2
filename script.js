993641607d41317e813ed18424c04f867ba085c8
sultan-4 <sultan.alhazmi11@gmail.com>
2022-04-05 05:20:49 +0300

    Revert "Update index.html"
    This reverts commit f31a88b95e702c1268c203cdc27b0d470cb1c868.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const liItem = document.querySelectorAll('ul li');
 const imgItem = document.querySelectorAll('.image img');
 
@@ -23,3 +24,36 @@ liItem.forEach(li => {
 });
 
 
+=======
+var liItem = document.querySelectorAll('ul li');
+var imgItem = document.querySelectorAll('.image img');
+
+liItem.forEach(li => {
+  li.onclick = function () {
+    liItem.forEach(li => {
+      li.className = "";
+    })
+    li.className = "all";
+    var value = li.textContent;
+    document.cookie = 'liselect=' + value 
+    
+    showImage(imgItem,value)
+  }
+});
+
+window.onload = function () {
+  if (document.cookie != undefined) {
+      showImage(imgItem,document.cookie.split("=")[1])
+      console.log(document.cookie)
+  }
+}
+function showImage (imgItem ,value){
+  imgItem.forEach(img => {
+    img.style.display = 'none';
+    if (img.getAttribute('data-filter') == value || value == "All") {
+      img.style.display = '';
+    }
+    
+  })
+}
+>>>>>>> parent of c539e0f (imgur api)
